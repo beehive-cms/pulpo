b04f19e1b3a12cdac86c98bb130ad5e8a4c0dada
mbajur <mbajur@gmail.com>
2018-11-10 19:45:11 +0100

    Move sidebar nav item to helper/partial

--- app/helpers/beehive/sidebar_helper.rb
+++ app/helpers/beehive/sidebar_helper.rb
@@ -1,26 +1,15 @@
 module Beehive
   module SidebarHelper
-    def sidebar_nav_item(label, uri, opts = {}, &block)
-      icon_class = opts[:text_icon].present? ? 'nav-item-icon--text' : nil
-
-      content_tag :li, nil, class: 'nav-item' do
-        capture do
-          concat(content_tag(:a, nil, href: uri, class: 'nav-link') do
-            capture do
-              concat(
-                content_tag(:div, nil, class: ['nav-item-icon', icon_class]) do
-                  if opts[:text_icon].present?
-                    content_tag :span, opts[:text_icon]
-                  elsif opts[:icon].present?
-                    content_tag :i, nil, class: "fa fa-#{opts[:icon]}"
-                  end
-                end
-              )
-              concat label
-            end
-          end)
-          concat content_tag :ul, capture(&block), class: 'nav flex-column' if block_given?
-        end
+    def sidebar_nav_item(title, uri, opts = {}, &block)
+      render(
+        layout: 'beehive/shared/sidebar_nav_item',
+        locals: {
+          title: title,
+          uri: uri,
+          opts: opts
+        }
+      ) do
+        capture(&block) if block_given?
       end
     end
   end
